9e6cd75bd16ecc4e109d05b0bb3506b5a520ac6b
paul-tes <pauasca8462@gmail.com>
2023-09-26 16:18:50 +0300

    Complete the devise jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, # => this is the devise_for :users that is used for the api
+                 controllers: {
+                   registrations: "api/v1/users/registrations",
+                   sessions: "api/v1/users/sessions",
+                 },
+                 path: "",
+                 path_names: {
+                   sign_in: "login",
+                   sign_out: "logout",
+                   registration: "signup",
+                 }
+    end
+  end
+
   namespace :api do
     namespace :v1 do
       resources :rooms, only: [:index, :show, :create, :update, :destroy] do
