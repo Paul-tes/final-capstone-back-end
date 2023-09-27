6f8b93267eb381ecd3e7636faa22af7769c24c39
paul-tes <pauasca8462@gmail.com>
2023-09-27 12:17:14 +0300

    Update user controller fix bugs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-  # Create a new user
-  def create
-    @user = User.new(user_params)
+  before_action :authenticate_user!
 
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # Get a list of all users
   def index
-    @users = User.all # current_user
-    render json: @users
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
+    render json: current_user, status: :ok
   end
 end
