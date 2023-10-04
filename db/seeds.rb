b09db13ac45ce564b0c82256e417a891e39c63d1
paul-tes <pauasca8462@gmail.com>
2023-10-04 13:58:51 +0300

    Fix broken image link

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,24 @@
 Room.create(
   room_number: "101",
   capacity: 4,
-  location: "Building A",
+  location: "Lasvegas Tomson",
   floor: "1st Floor",
-  img: "room1.jpg",
+  img: "https://media.cnn.com/api/v1/images/stellar/prod/140127103345-peninsula-shanghai-deluxe-mock-up.jpg",
   price_per_day: 100.00,
   description: "Cozy room with a view",
   comment: "Great room for a comfortable stay",
   available: true,
   rating: 4,
-  hosted_by: "Host ABC",
+  hosted_by: "LasVegas Tomson toneto",
   likes: 10
 )
 
 Room.create(
   room_number: "201",
   capacity: 2,
-  location: "Building B",
+  location: "Ingland",
   floor: "2nd Floor",
-  img: "room2.jpg",
+  img: "https://cdn.loewshotels.com/loewshotels.com-2466770763/cms/cache/v2/5f5a6e0d12749.jpg/1920x1080/fit/80/86e685af18659ee9ecca35c465603812.jpg",
   price_per_day: 150.00,
   description: "Spacious and modern room",
   comment: "Highly recommended for a luxurious stay",
@@ -32,9 +32,9 @@ Room.create(
 Room.create(
   room_number: "301",
   capacity: 3,
-  location: "Building C",
+  location: "Lasvegas Mthona",
   floor: "3rd Floor",
-  img: "room3.jpg",
+  img: "https://cdn.loewshotels.com/loewshotels.com-2466770763/cms/cache/v2/5f5a6e0d12749.jpg/1920x1080/fit/80/86e685af18659ee9ecca35c465603812.jpg",
   price_per_day: 120.00,
   description: "Elegant room with a balcony",
   comment: "Perfect for a relaxing getaway",
