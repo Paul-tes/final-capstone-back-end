a6b68970ca0475819e46c0a85cbe72003c6d7b8b
Nasir <nasirbhatti.nk@gmail.com>
2023-10-05 17:19:03 +0500

    Update code for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# Apache or NGINX already handles this.
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
